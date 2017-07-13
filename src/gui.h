a129d928a017a048b3962d94cb8e633c97033210
Sergio Basurco <sergio.basurco@gmail.com>
2017-07-13 19:00:14 +0200

    added gui class.

---
+++ src/gui.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "ofxGui.h"
+
+class ShaftGui
+{
+        ofParameter<ofColor> baseColor;
+    	ofParameter<ofColor> accentColor;
+    	ofParameter<ofColor> sunColor;
+    	ofParameter<float> sunRadius;
+    	ofParameter<float> decay;
+    	ofParameter<float> exposure;
+    	ofParameter<float> density;
+    	ofParameter<float> weight;
+    	ofParameter<int> numSamples;
+    	ofParameter<int> sunResolution;
+    	ofParameter<bool> moveSun;
+    	ofxButton loadImageButton;
+    	ofxButton saveSnapshotButton;
+
+		ofParameterGroup parameters;
+		ofxPanel gui;
+
+    public:
+        void draw();
+        void loadImageButtonPressed();
+        void sunResolutionChanged(int& circleResolution);
+	    void saveSnapshot();
+
+        float const & getSunRadius() const;
+        float const & getDecay() const;
+        float const & getExposure() const;
+        float const & getDensity() const;
+        float const & getWeight() const;
+        int const & getNumSamples() const;
+        int const & getSunResolution() const;
+        bool const & isSunMoving() const;
+
+        
+}
